333237460c0fed730f45f7989a68a7a16bc844f2
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-12 16:01:26 +0200

    update task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -67,5 +67,7 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	quick_sort_recursive(array, 0, size - 1, size);
+	int start = 0, end = size - 1;
+
+	quick_sort_recursive(array, start, end, size);
 }
