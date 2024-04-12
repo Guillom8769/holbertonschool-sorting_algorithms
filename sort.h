a6fa1133907b21c5d3e3e1603fceae527a19c8af
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-12 15:44:00 +0200

    update header file

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int start, int end, int size);
+int lomuto_partition(int *array, int start, int end, int size);
+
 
 #endif
